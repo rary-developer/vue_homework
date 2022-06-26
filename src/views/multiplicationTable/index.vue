68fc26ee20e4e41f1f72bdc87ec99ed1ca94e9ac
rary <xogml6915@gmail.com>
2022-06-26 11:34:34 +0900

    1st homework

--- src/views/multiplicationTable/index.vue
+++ src/views/multiplicationTable/index.vue
@@ -1,22 +1,32 @@
 <template>
   <div>
     <h1> <span style="cursor: pointer" @click="$router.go(-1)">&lt;</span>&nbsp;&nbsp; 구구단</h1>
+    <div style="display: flex;">
+      <div>
+        <input type="number" v-model="num">단 <!--숫자 적는 부분-->
+      </div>
+      <br>
+      <div>
+        <ul v-if="num !== ''" style="list-style: none"> <!--num에 값이 있을때..-->
+          <li v-for="i in 9" :key="i"> <!--i가 9까지 key가 id v-for에 key값 넣기-->
+            {{num}} X {{i}} = {{num*i}}
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
-
+<!--esLint 도대체 뭘까 띄워쓰기 하나 잘못하면 에러를 만들어내네..-->
 <script>
-/** *
- * 1. data만 사용할 것
- */
 export default {
   name: 'multiplication_table',
   data() {
     return {
+      num: '',
     };
   },
 };
 </script>
 
 <style scoped>
-
 </style>
